f3f34209b394ad06671a7a63bf9c0a38ae711a3d
bobby060 <scoutrpn@gmail.com>
2018-10-31 14:55:21 -0400

    All done except unblocking users

--- Project 2/chatClientShell.py
+++ Project 2/chatClientShell.py
@@ -11,7 +11,7 @@ Author: Robert Norwood, Amanda Roper
 Date: 
 Description:  Sample chat client for CS484 programming assignment
 """
-blockedNames = []
+blockedNames = list()
 helpMessage = """
     Possible commands: \n
     #user <handle>: changes a user handle\n
@@ -22,15 +22,14 @@ helpMessage = """
 """
 
 
-def readFromServer(clientSocket):
+def readFromServer(clientSocket, stop_event):
 
  
   # You need to receive input from client socket and print to screen.
-  while (1):
+  while (not stop_event.is_set()):
     # TODO: add timestamp
     # TODO check blocked usernames
     message = clientSocket.recv(1024)
-    print(blockedNames)
 
     message = message.decode()
     curDate = datetime.strftime(datetime.today(),"%a, %d %b %Y %H:%M:%S %Z")
@@ -69,8 +68,11 @@ print(helpMessage)
 #   2. Connect to server using client socket
 #   3. Create a reader thread object
 #       Threading tips: https://pymotw.com/2/threading/
+kill = threading.Event()
+# https://stackoverflow.com/questions/18018033/how-to-stop-a-looping-thread-in-python
+# Used to figure out how to use an event to stop the receving thread. West Point, NY. 31 OCT 18
 
-t = threading.Thread(target=readFromServer, args=(clientSocket, ))
+t = threading.Thread(target=readFromServer, args=(clientSocket,kill, ))
 t.start()
 
 
@@ -81,7 +83,6 @@ flag = 1
 while (flag == 1):
     outMessage = input()
     if (outMessage[:5]=="#help"):
-        print(outMessage[:5])
         print(helpMessage)
     elif (outMessage[:5]=="#user"):
         username2=outMessage[6:]
@@ -89,16 +90,22 @@ while (flag == 1):
         username=username2
         clientSocket.send(message.encode())
     elif (outMessage[:6]=="#block"):
-        print(outMessage[7:])
+        print(outMessage[7:] + " has been blocked")
         c = ""
-        blockedNames+=c.join(outMessage[7:])
-    elif (outMessage[:7]=="#unblock"):
-        c = ""
-        blockedNames-=c.join(outMessage[8:])
+        # https://www.dotnetperls.com/string-list-python
+        # used to realize I need to use .append not += otherwise it screws thigns up
+        blockedNames.append(outMessage[7:])
+    elif (outMessage[:8]=="#unblock"):
+        print (outMessage[9:]+ " has been removed from blocklist")
+        blockedNames.remove(outMessage[9:])
+        #TODO this doesn't actually remove the name from the blocklist
     elif (outMessage[:4]=="#bye"):
         message = username+" has left the chat."
         clientSocket.send(message.encode())
-        sys.exit()
+        kill.set()
+        t.join()
+        flag = 0
+        exit()
         # TODO Quit only kind of kills the program. Need something besides exit.
 
 
